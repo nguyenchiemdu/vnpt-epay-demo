c8dc33cbe013b1c6c53da58b4984270da36407bd
nguyenchiemdu <nnguenchiemdu@gmail.com>
2023-10-25 10:55:31 +0700

    static site code for payment

---
+++ generator/merchantToken.js
@@ -0,0 +1,16 @@
+// const crypto = require('crypto');
+
+// function calculateSHA256Hash(timeStamp, merTrxId, merId, amount, encodeKey) {
+//   const dataToHash = timeStamp + merTrxId + merId + amount + encodeKey;
+//   const hash = crypto.createHash('sha256').update(dataToHash).digest('hex');
+//   return hash;
+// }
+
+// const timeStamp = "20231024133207";
+// const merTrxId = "DEMO_20231024133207_8951";
+// const merId = "MGPDEMO001";
+// const amount = "3000000";
+// const encodeKey = "yourEncodeKey"; // Replace with your actual encode key
+
+// const sha256Hash = calculateSHA256Hash(timeStamp, merTrxId, merId, amount, encodeKey);
+// console.log(sha256Hash);
